b9af2612ae40dfde7cc2c5b0fb5cf91b05b892db
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-09-19 01:38:26 +0000

    Edit getAllProperties query function to accept filtering and fetch from SQL db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -96,14 +96,47 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-    return pool.query(`
-    SELECT * FROM properties
-    LIMIT $1`,
-    [limit])
-      .then(res => res.rows)
-      // .catch(rej => rej.err);
+  const queryParams = [];
+  let queryString = `
+    SELECT properties.*, avg(rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON property_id = properties.id `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
   }
+
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryParams.length > 1 ? queryString += `AND ` : queryString += `WHERE `;
+    queryString += `owner_id = ${queryParams.length}`
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`, `${options.maximum_price_per_night}`);
+    queryParams.length > 2 ? queryString += `AND ` : queryString += `WHERE `;
+    queryString += `cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length} `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryParams.length > 1 ? queryString += `AND ` : queryString += `WHERE `;
+    queryString += `rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows)
+}
 exports.getAllProperties = getAllProperties;
+
 
 
 /**
